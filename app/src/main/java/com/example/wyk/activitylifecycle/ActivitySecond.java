ad43ea2a9eeee7a2f41dfed9813a7d51061354b1
wyk <051092296wyk@163.com>
2016-07-22 10:56:50 +0800

    activity第一次提交

---
+++ app/src/main/java/com/example/wyk/activitylifecycle/ActivitySecond.java
@@ -0,0 +1,9 @@
+package com.example.wyk.activitylifecycle;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by lsx on 2016/7/22.
+ */
+public class ActivitySecond extends AppCompatActivity {
+}
---
+++ app/src/main/java/com/example/wyk/activitylifecycle/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.wyk.activitylifecycle;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d(TAG, "onCreate: ");
+        Button button = (Button) findViewById(R.id.activity_main_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ActivitySecond.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart: ");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume: ");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause: ");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop: ");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy: ");
+    }
+}
